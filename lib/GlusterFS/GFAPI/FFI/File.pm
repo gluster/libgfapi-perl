c51c108671839c3c8e0f647d77c922333bdfc982
Ji-Hyeon Gim <potatogim@potatogim.net>
2018-02-06 10:01:27 +0900

    Update LICENSE
    removes un-necessary description

--- lib/GlusterFS/GFAPI/FFI/File.pm
+++ lib/GlusterFS/GFAPI/FFI/File.pm
@@ -263,10 +263,9 @@ Ji-Hyeon Gim E<lt>potatogim@gluesys.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2017 by Ji-Hyeon Gim.
+This software is copyright 2017-2018 by Ji-Hyeon Gim.
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+This is free software; you can redistribute it and/or modify it under the same terms as the GPLv2/LGPLv3.
 
 =cut
 
